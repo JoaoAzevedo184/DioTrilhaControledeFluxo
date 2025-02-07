88b11043f620697166968f37fb6aa3830899e920
JoaoAzevedo184 <ccjconrad50@gmail.com>
2025-02-07 16:29:54 -0300

    Desafio Feito.

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
-
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner terminal = new Scanner(System.in);
+        boolean sucesso = false;
+        do {
+            try {
+                System.out.print("Digite o primeiro parâmetro: ");
+                int parametroUm = terminal.nextInt();
+                System.out.println(" ");
+                System.out.print("Digite o segundo parâmetro: ");
+                int parametroDois = terminal.nextInt();
+                System.out.println(" ");
+                //chamando o metodo contendo a lógica de contagem
+                contar(parametroUm, parametroDois);
+                sucesso = true;
+            }catch (ParametrosInvalidosException e) {
+                System.out.println("Erro: O segundo parâmetro deve ser maior que o primeiro.");
+            } catch (Exception e) {
+                System.out.println("Erro: Entrada inválida! Certifique-se de digitar números inteiros.");
+                terminal.nextLine(); // Limpa o buffer do scanner para evitar loop infinito
+            }
+        }while(!sucesso);
+    }
+    public static class ParametrosInvalidosException extends Exception{
+        public ParametrosInvalidosException() {
+            super("O segundo parâmetro deve ser maior que o primeiro.");
         }
     }
-}
+
+    public static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
+        //validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if(parametroUm>=parametroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = parametroDois - parametroUm;
+        //realizar o for para imprimir os números com base na variável contagem
+        for(int i=1;i<=contagem;i++){
+            System.out.printf("Imprimindo o numero %d\n",i);
+        }
+    }
+    }
